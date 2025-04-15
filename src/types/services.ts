912c123568228d8a9b3de33f388f1eff9e8ec132
LinKDx <omar_150596@hotmail.com>
2025-04-15 10:27:37 -0600

    creating landing page

---
+++ src/types/services.ts
@@ -0,0 +1,12 @@
+
+
+export interface Servicios {
+    id: string;
+    nombre: string;
+    descripcion: string;
+    duracion: string;
+    precio: number;
+    img: string;
+    modalidad: string[];
+    areasEnfoque: string[];
+}
